6bf44f58dd75c107c822aa649096e7a36fbe1adf
Ben Koren <benkoren@gmail.com>
2017-09-27 17:32:33 -0500

    Chapter 6 progress. Next up: 6.3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,8 +4,51 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "test@example.com")
   end
-  
+
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
+
+  test "email should only accept valid addresses" do
+    valid_addresses = %w[user@example.com USEr@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should be saves as downcase" do
+    mixed_email = "FoO@bAr.CoM"
+    @user.email = mixed_email
+    @user.save
+    assert_equal mixed_email.downcase, @user.reload.email
   end
 end
